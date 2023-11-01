f88552e513ac779f30b4ba77fa42dddb3cd80f8b
ShuangXu <sxuu0044@gmail.com>
2023-11-01 13:48:39 +1100

    integration

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
 using System.Globalization;using Microsoft.EntityFrameworkCore;
 using GeekGallery.Services;
 using GeekGallery.Data;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<ApiContext>(opt => opt.UseInMemoryDatabase("PostsDb"));
 //builder.Services.AddSingleton<DataService>();
-// 注册 AuthorDbContext，用于处理Author数据库
+
 //builder.Services.AddDbContext<ApiContext>(opt => opt.UseInMemoryDatabase("AuthorsDb"));
 
 builder.Services.AddControllers();
@@ -14,7 +17,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Json Serializer
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore).AddNewtonsoftJson(
+    options=>options.SerializerSettings.ContractResolver=new DefaultContractResolver());
+
 var app = builder.Build();
+
+//Enable CORS
+app.UseCors(c => c.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
