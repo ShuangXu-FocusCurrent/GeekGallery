619f3ab71bb24b24dccf056a159b20fa35cf3eac
ShuangXu <sxuu0044@gmail.com>
2023-11-06 18:04:22 +1100

    mysql with bug

--- Program.cs
+++ Program.cs
@@ -1,16 +1,32 @@
+using System.Configuration;
 using System.Globalization;using Microsoft.EntityFrameworkCore;
 using GeekGallery.Data;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<ApiContext>(opt=>opt.UseInMemoryDatabase("PostsDb"));
+
+builder.Services.AddDbContext<ApiContext>(options =>
+{
+    IConfiguration configuration = builder.Configuration;
+    options.UseMySql(
+        configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8, 0, 35)));
+});
+
 builder.Services.AddControllers();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+//Enable CORS
+app.UseCors(c => c.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
